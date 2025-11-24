7edcb9cf3a06f65d1ae9a3b0f69de0823bc46ab0
TK911-LG <144928859+TK911-LG@users.noreply.github.com>
2025-11-24 14:14:39 +0700

    Delete Frontend_1 directory

--- Frontend_1/src/components/ui/Card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card({ children, className = '', title = null }) {
-    return (
-        <div className={`card ${className}`}>
-            {title && <h3 className="card-title">{title}</h3>}
-            <div className="card-content">{children}</div>
-        </div>
-    )
-}
